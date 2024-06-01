98ca776e3ba7c103eef15a8b017dabb069ba7e22
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-01 08:48:27 +0200

    add script file and modified input field and its css

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hello World")
+const minPrice =document.getElementById("min-price");
+const mxPrice =document.getElementById("mx-price");
+const priceRange =document.getElementById("price-range");
+const output =document.getElementById("output");
+
+let minPriceValue =Number(minPrice.value);
+let mxPriceValue =Number(mxPrice.value);
+
+
+console.log(typeof minPriceValue)
+console.log(typeof mxPriceValue)
