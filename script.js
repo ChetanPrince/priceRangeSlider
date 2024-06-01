317242f01d33c0fa28927deae773317b0eefdbe1
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-01 19:08:07 +0200

    slider grabbing input values and output percent to show shaded area of slider

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-console.log("Hello World")
-const minPrice =document.getElementById("min-price");
-const mxPrice =document.getElementById("mx-price");
-const priceRange =document.getElementById("price-range");
-const output =document.getElementById("output");
-const minPriceCircle =document.getElementById("price-range1");
-const maxPriceCircle =document.getElementById("price-range2");
+// range input grabs all input values from slider in a single query selector const 
+const rangeInput = document.querySelectorAll(".rangeContainer input");
+
+// for each  "input" (input as the first argument to perform an action upon it) a function/event is parsed as an arrow function 
+rangeInput.forEach(input =>{
+    // input event is listened on input element and empty parameter to be parsed to perform arrow function
+    input.addEventListener("input", ()=>{
+
+        // getting two ranges values and parsing them to number
+        let minVal=  parseInt(rangeInput[0].value),
+        maxVal = parseInt(rangeInput[1].value);
+        // percent to grab a value where rangeInput[0].max to get 4 digits upmost number 
+
+        let percent = (minVal / rangeInput[0].max)*100;
 
 
-let minPriceValue =Number(minPrice.value);
-let mxPriceValue =Number(mxPrice.value);
 
+        
+        // console.log(minVal, maxVal);
+        console.log(percent);
+    })
+})
 
-let priceGap = 100;
 
 
 
 // How to grab values of  minPriceCircle when sliding left to right between 1 and 9000?
+    // event listener on inputs in the slider div to select queries and then parsed them to extract integer from their values
+
+
 //minPriceCircle has to be set to minPriceValue input box and vice versa
     // an input is set and between 1 and 9000 as there is a gap of 1000 and minPriceInput is restricted to go above 9000 and below 1
     // when minPriceCircle is slided it shows a placeholder with the value and simultaneously minPriceValue input is set to that value
@@ -26,7 +38,3 @@ let priceGap = 100;
 
 
 
-
-
-console.log(typeof minPriceValue)
-console.log(typeof mxPriceValue)
