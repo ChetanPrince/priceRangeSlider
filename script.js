a47626fc229184495428efc63918207d7ff608e6
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-02 10:12:43 +0200

    slider and price input events added

--- script.js
+++ script.js
@@ -1,40 +1,76 @@
-// range input grabs all input values from slider in a single query selector const 
-const rangeInput = document.querySelectorAll(".rangeContainer input");
-
-// for each  "input" (input as the first argument to perform an action upon it) a function/event is parsed as an arrow function 
-rangeInput.forEach(input =>{
-    // input event is listened on input element and empty parameter to be parsed to perform arrow function
-    input.addEventListener("input", ()=>{
-
-        // getting two ranges values and parsing them to number
-        let minVal=  parseInt(rangeInput[0].value),
-        maxVal = parseInt(rangeInput[1].value);
-        // percent to grab a value where rangeInput[0].max to get 4 digits upmost number 
-
-        let percent = (minVal / rangeInput[0].max)*100;
-
-
-
-        
-        // console.log(minVal, maxVal);
-        console.log(percent);
-    })
-})
-
-
-
 
 // How to grab values of  minPriceCircle when sliding left to right between 1 and 9000?
-    // event listener on inputs in the slider div to select queries and then parsed them to extract integer from their values
+// event listener on inputs in the slider div to select queries and then parsed them to extract integer from their values
 
 
 //minPriceCircle has to be set to minPriceValue input box and vice versa
-    // an input is set and between 1 and 9000 as there is a gap of 1000 and minPriceInput is restricted to go above 9000 and below 1
-    // when minPriceCircle is slided it shows a placeholder with the value and simultaneously minPriceValue input is set to that value
-    // if minPriceCircle is slided to less than price gap then automatically maxPriceCircle is set to minPriceValue+priceGap
-    
+// an input is set and between 1 and 9000 as there is a gap of 1000 and minPriceInput is restricted to go above 9000 and below 1
+// when minPriceCircle is slided it shows a placeholder with the value and simultaneously minPriceValue input is set to that value
+// if minPriceCircle is slided to less than price gap then automatically maxPriceCircle is set to minPriceValue+priceGap
+
 
 // maxPriceCircle has to be set to maxPriceValue input box and vice versa
+
+
+
+ 
+const rangeInput = document.querySelectorAll(".rangeContainer input");
+const progress= document.querySelector(".slider .progress");
+let minValueGap = 1500;
+const priceInput = document.querySelectorAll(".block input");
+
+// rangeSlider with slider Option
+priceInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minVal=  parseInt(priceInput[0].value),
+        maxVal = parseInt(priceInput[1].value);
+        
+        if((maxVal-minVal >= minValueGap) && maxVal<=10000 && minVal>=0){
+            if(e.target.className ==="min-price"){
+                rangeInput[0].value = minVal;
+                progress.style.left = (minVal / rangeInput[0].max)*100 + "%";
+            }
+                else{
+                    rangeInput[1].value = maxVal;
+                    progress.style.right = ((rangeInput[0].max - maxVal) / rangeInput[1].max)*100 + "%";
+                }
+                
+        }
+
+})
+});
+
+
+
+
+// rangeSlider with slider Option
+
+rangeInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minVal=  parseInt(rangeInput[0].value),
+        maxVal = parseInt(rangeInput[1].value);
+        
+        if(maxVal-minVal < minValueGap){
+            if(e.target.className ==="min-range"){
+                rangeInput[0].value = maxVal-minValueGap;}
+                else{
+                    rangeInput[1].value = minVal + minValueGap;
+                }
+                
+            }
+else{
+     
+    priceInput[0].value = minVal;
+    priceInput[1].value =maxVal;
+    progress.style.left = (minVal / rangeInput[0].max)*100 + "%";
+    progress.style.right = ((rangeInput[0].max - maxVal) / rangeInput[1].max)*100 + "%";
+}
+})
+});
+
+
+
+
 
 
 
