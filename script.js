cf4df4dbf4e1a4ab1362102dbda4753099064ab6
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-04 09:12:20 +0200

    set min and max to not be below 0 and 1000

--- script.js
+++ script.js
@@ -1,46 +1,51 @@
-//  negative numbers bug should not be allowed?
-// range lower than 4 numbers should not produce negative numbers
-//  also what if someone tries to enter 3 digit 
-
-
-
-
-
 const rangeInput = document.querySelectorAll(".rangeContainer input");
 const progress= document.querySelector(".slider .progress");
 let minValueGap = 1000;
 const priceInput = document.querySelectorAll(".block input");
+let min = 0;
+let max = 1000;
 
-// rangeSlider with slider Option
 priceInput.forEach(input =>{
     input.addEventListener("blur", e =>{
-        e.preventDefault();
+       
         let minPrice=  parseInt(priceInput[0].value),
         maxPrice = parseInt(priceInput[1].value);
-        
-        if((maxPrice-minPrice >= minValueGap) && maxPrice<=rangeInput[1].max && minPrice>=rangeInput[0].min){
-            if(e.target.className ==="min-price"){
-                rangeInput[0].value = minPrice;
-                progress.style.left = ((minPrice / rangeInput[0].max)*100) + "%";
-            }
-            else{
-                rangeInput[1].value = maxPrice;
-                progress.style.right =100-( maxPrice / rangeInput[1].max)*100 + "%";
-            }}
-        else if(maxPrice - minPrice < minValueGap){  
-             if(e.target.className ==="min-price"){
-                 maxPrice = minPrice + minValueGap;
-                 priceInput[1].value = maxPrice;
-                 rangeInput[0].value = minPrice;
-                progress.style.left = ((minPrice / rangeInput[0].max)*100) + "%";}
-             else{
-                minPrice = maxPrice - minValueGap;
-                priceInput[0].value = minPrice;
-                rangeInput[1].value = maxPrice;
-                progress.style.right =100-( maxPrice / rangeInput[1].max)*100 + "%";
-      
-             }}
+        if (minPrice < 0) {
+            minPrice = 0;
+            priceInput[0].value = minPrice;
+        }
     
+        // Ensure maxPrice is at least 1000, but allow user to type more than 1000
+        if (maxPrice < 1000 && e.target.className === "max-price") {
+            maxPrice = 1000;
+            priceInput[1].value = maxPrice;
+        }
+
+            if((maxPrice-minPrice >= minValueGap) && maxPrice<=rangeInput[1].max && minPrice>=rangeInput[0].min){
+                if(e.target.className ==="min-price"){
+                    rangeInput[0].value = minPrice;
+                    progress.style.left = ((minPrice / rangeInput[0].max)*100) + "%";
+                }
+                else{
+                    rangeInput[1].value = maxPrice;
+                    progress.style.right =100-( maxPrice / rangeInput[1].max)*100 + "%";
+                }}
+            else if(maxPrice - minPrice < minValueGap){  
+                 if(e.target.className ==="min-price"){
+                     maxPrice = minPrice + minValueGap;
+                     priceInput[1].value = maxPrice;
+                     rangeInput[0].value = minPrice;
+                    progress.style.left = ((minPrice / rangeInput[0].max)*100) + "%";}
+                 else{
+                    minPrice = maxPrice - minValueGap;
+                    priceInput[0].value = minPrice;
+                    rangeInput[1].value = maxPrice;
+                    progress.style.right =100-( maxPrice / rangeInput[1].max)*100 + "%";
+          
+                 }
+                 console.log(minPrice, maxPrice, rangeInput[0].value, rangeInput[1].value)
+        }
+            
         })})
 
 
