c98a11d0bff77e56941389885bfc9d389f7fad01
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-05 19:15:54 +0200

    bug need to be taken care of where maxPrice has to set minPrice and vice versa for a min valuegap

--- script2.js
+++ script2.js
@@ -6,6 +6,43 @@ let max = 1;
 const progress = document.querySelector(".slider .progress");
 
 
+
+// adding loop in priceInput to update slider according to price
+
+priceInput.forEach(input =>{
+    input.addEventListener("blur", e =>{
+        let minPrice = parseInt(priceInput[0].value),
+        maxPrice =parseInt(priceInput[1].value);
+        // condition when price difference is below 1000 then checking which element if being input in and accordingly set other value to be off of minValGap
+        let maxPriceLength = priceInput[1].value.length;
+
+        if(maxPriceLength >= 4){
+            if(maxPrice - minPrice < minValGap){
+                if(e.target.className="minPrice"){
+                    priceInput[1].value = minPrice + minValGap;
+                    }
+                else{
+                    priceInput[0].value = maxPrice - minValGap;
+                } }
+            else{
+                    let percent1 = (priceInput[0].value/100)+"%";
+                    let percent2 = ((priceInput[1].max - rangeInput[1].value) / 100)+"%";
+                    progress.style.left = percent1;
+                    progress.style.right = percent2;
+                    rangeInput[0].value = priceInput[0].value;
+                    rangeInput[1].value = priceInput[1].value;
+                }
+        }
+        else{
+             alert("Kindly have a minimum input of 0 at Min Price and 1000 at MaxPrice");
+             return;
+        }
+    })
+})
+
+
+
+
 // adding function to check range inputs and setting left and right properties of progress style
 
 rangeInput.forEach(input =>{
@@ -25,13 +62,9 @@ rangeInput.forEach(input =>{
         else{
             let percent1 = (rangeInput[0].value/100)+"%";
             let percent2 = ((rangeInput[1].max - rangeInput[1].value) / 100)+"%";
-            console.log(percent2);
             progress.style.left = percent1;
             progress.style.right = percent2;
             priceInput[0].value = rangeInput[0].value;
             priceInput[1].value = rangeInput[1].value;
-
         }
-
-
 })})
